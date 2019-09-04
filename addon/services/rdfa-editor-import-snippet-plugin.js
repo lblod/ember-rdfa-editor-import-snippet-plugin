82b18da25ebd17fc4fab756fc6a589ce9790f66b
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-09-04 07:37:58 +0200

    updated hinting flow

--- addon/services/rdfa-editor-import-snippet-plugin.js
+++ addon/services/rdfa-editor-import-snippet-plugin.js
@@ -33,55 +33,80 @@ const RdfaEditorImportSnippetPlugin = Service.extend({
     let myHints = hintsRegistry.getHintsFromPlugin(this.get('who'));
 
     const hints = [];
+
+    /*
+     * When the resource matches a snippet, we want the region of the toplevel node describing the resource hinted.
+     * Basically the capital 'R' nodes:
+     *                       o
+     *                      /  \
+     *        This one <--  R   o
+     *                     /|\  |\
+     *                    r r r o R  -> This one
+                                  /\
+     *                           r  r
+     * Note: this is not going to work for self referring (nested) resources.
+     */
     for(let context of contexts){
       let potentialContext = context.context.slice(-1)[0] || {}; // We take the most specific subject of the context.
       let snippets = this.snippetsForContext(potentialContext); //TODO: assumes the snippets download is ready.
       if (snippets.length === 0 ) continue;
 
       let semanticNodeLocation = [ context.semanticNode.start, context.semanticNode.end ];
-      let updatedSemanticNodeLocation = hintsRegistry.updateLocationToCurrentIndex(hrId, semanticNodeLocation);
 
       // We store largest region from hint, so there can only be one hint with region containing other regions for a resource.
-      let existingHintContainingLocation = this.getHintContainingLocationForResource(myHints, updatedSemanticNodeLocation, potentialContext.subject);
+      //TODO: I assume the hrId hasn't evolved since this function is a sync block
+      // Note: max one hint is expected, since we store only the biggest region
+      let existingHintContainingLocation = this.getHintContainingLocationForResource(myHints, semanticNodeLocation, potentialContext.subject);
       let newHintContainingLocation = this.getHintContainingLocationForResource(hints, semanticNodeLocation, potentialContext.subject);
 
+      // Case: we have found hints in the registry which contains current location
+      // Then take this location and add the latest state of snippets data to it.
+      if(existingHintContainingLocation && !newHintContainingLocation){
+        hintsRegistry.removeHintsAtLocation(existingHintContainingLocation.location, hrId, this.get('who'));
+        hints.push(this.generateCard(hrId, hintsRegistry, editor, existingHintContainingLocation.location, 'Nieuwe snippets', snippets));
+        continue;
+      }
+
       // Case: no bigger regions have been found, not in the hintsRegistry, nor in the newly created hints
-      // We can simply add the hint.
+      // If the currentLocation contains the one in the registry, we clean the registry.
+      // After, we simply add the hint.
       if(!existingHintContainingLocation && !newHintContainingLocation){
-        hintsRegistry.removeHintsInRegion(semanticNodeLocation, hrId, this.get('who'));
+
+        // Check if the new location does contains contain hints from registry
+        // Note: this check is not needed for newHintContainingLocation, since context scanner goes depth first.
+        // Note: multiple hints may be contained by the new location. (see. e.g. topNode in example doc above)
+        let containedHints = this.getHintsForResourceContainedByLocation(myHints, semanticNodeLocation, potentialContext.subject);
+        if(containedHints.length > 0){
+          containedHints.forEach(h => hintsRegistry.removeHintsAtLocation(h.location, hrId, this.get('who')));
+        }
+        hintsRegistry.removeHintsAtLocation(semanticNodeLocation, hrId, this.get('who'));
         hints.push(this.generateCard(hrId, hintsRegistry, editor, semanticNodeLocation, 'Nieuwe snippets', snippets));
         continue;
       }
 
-      // Case: we have found hints in the registry which contains current location
-      // Then append the new snippet data to the hint
-      if(existingHintContainingLocation && !newHintContainingLocation){
-        let updatedSnippets = this.appendSnippets(existingHintContainingLocation.info.snippets, snippets);
-        hintsRegistry.removeHintsInRegion(existingHintContainingLocation.location, hintsRegistry.currentIndex(), this.get('who'));
-        hints.push(this.generateCard(hintsRegistry.currentIndex(), hintsRegistry, editor, existingHintContainingLocation.location, 'Nieuwe snippets', updatedSnippets));
+      // Case: we have found new hint wich contains current location
+      // We do nothing, the bigger hint wins and will be inserted
+      if(newHintContainingLocation){
+        continue;
       }
 
-      // if(newHintContainingLocation) => means hint will be pushed after current loop, no further action is needed.
-      // Not possible there is an additionnal snippet since the state of importRdfaSnippet didn't change
-      // during the exection of this method
     }
+
     if(hints.length > 0){
       hintsRegistry.addHints(hrId, this.get('who'), hints);
     }
   }),
 
   getHintContainingLocationForResource(hints, location, resource){
-    return hints.filter(h => h.location[0] <= location[0] && location[1] <= h.location[1] && h.info.resource === resource)[0];
+    return hints.filter(h => this.containsLocation(h.location, location) && h.info.resource === resource)[0];
   },
 
-  appendSnippets(existingSnippets, newSnippets){
-    let mergedSnippets = existingSnippets;
-    newSnippets.forEach(ns => {
-      if(!existingSnippets.find(es => ns.snippet == es.snippet)){
-        mergedSnippets.pushObject(ns);
-      }
-    });
-    return mergedSnippets;
+  getHintsForResourceContainedByLocation(hints, location, resource){
+    return hints.filter(h => this.containsLocation(location, h.location) && h.info.resource === resource);
+  },
+
+  containsLocation(refLocation, testLocation){
+      return refLocation[0] <= testLocation[0] && testLocation[1] <= refLocation[1];
   },
 
   /**
